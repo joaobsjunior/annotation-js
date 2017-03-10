5df6ab748b6101b086065443d9b7b72e926e0c8f
João B.S. Júnior <joaojunior.mail@gmail.com>
2017-03-10 19:37:52 -0300

    Adicionado opção de sufixo para objetos internos de uma class.

--- annotation.js
+++ annotation.js
@@ -1,8 +1,9 @@
 /*
 	@Annotation(
-		DATA[queyr=""<required>,column=""<opcional>,type=""<opcional>]
+		DATA[query=""<required>,column=""<opcional>,type=""<opcional>,sufix=""<string for sufix>]
 	)
 	@type{class,number,date,boolean,string}
+    @sufix{<string for sufix>}
 */
 
 JSON.clone = function (value) {
@@ -17,7 +18,7 @@ Object.prototype.getAnnotations = function (_typeQueryEnum = "DATA", _maxLevel =
     var objects = {};
     var maxLevel = _maxLevel;
 
-    var getObjects = function (_self, _keyObjArray, _level = 1) {
+    var getObjects = function (_self, _keyObjArray, _level = 1, _sufix = "") {
         var self = _self;
         var level = _level;
         var keyObjArray = _keyObjArray || new Array();
@@ -27,11 +28,22 @@ Object.prototype.getAnnotations = function (_typeQueryEnum = "DATA", _maxLevel =
         annotations.forEach(function (value, index) {
             value = value.replace(/],\)/g, "])");
             var keyObj = /this.(.*?)(\=)/.exec(value)[1];
-            var valueType = /@type.(.*?)(\})/.exec(value)[1].toLowerCase();
+            var valueType = /@type.(.*?)(\})/.exec(value);
+            if (valueType && valueType.length === 3) {
+                valueType = valueType[1].toLowerCase();
+            } else {
+                valueType = "string";
+            }
+            var sufix = /@sufix.(.*?)(\})/.exec(value);
+            if (sufix && sufix.length === 3) {
+                sufix = _sufix + sufix[1];
+            } else {
+                sufix = _sufix;
+            }
             var keyObjArr = JSON.clone(keyObjArray);
             keyObjArr.push(keyObj);
             if (valueType === "class" && level < maxLevel) {
-                getObjects(self[keyObj], keyObjArr, level + 1);
+                getObjects(self[keyObj], keyObjArr, level + 1, sufix);
             } else if (valueType !== "class") {
                 var annotationsType = /\(((\S)*?)\]\)/g.exec(value)[1].split("],");
                 var annotationsParamns = null;
@@ -40,7 +52,7 @@ Object.prototype.getAnnotations = function (_typeQueryEnum = "DATA", _maxLevel =
                     if (annotationsType.hasOwnProperty(key) && annotationsType[key].match(typeQueryEnum)) {
                         annotationsParamns = /\[(\S*)/g.exec(annotationsType[key])[1];
                         annotationsParamns = annotationsParamns.match(/(\"(.*?)\")|\,|(\S.*?)\=/g);
-                        objectGenerate = objectGeneration(annotationsParamns);
+                        objectGenerate = objectGeneration(annotationsParamns, _sufix);
                         break loop1;
                     }
                 }
@@ -48,7 +60,7 @@ Object.prototype.getAnnotations = function (_typeQueryEnum = "DATA", _maxLevel =
                     var keyObjArrJoin = keyObjArr.join(".");
                     objects[keyObjArrJoin] = objectGenerate;
                     if (!objects[keyObjArrJoin].type) {
-                        objects[keyObjArrJoin].type = valueType || "string";
+                        objects[keyObjArrJoin].type = valueType;
                     }
                     if (!objects[keyObjArrJoin].dir) {
                         objects[keyObjArrJoin].dir = "BIND_IN";
@@ -58,15 +70,20 @@ Object.prototype.getAnnotations = function (_typeQueryEnum = "DATA", _maxLevel =
         });
     }
 
-    var objectGeneration = function (_annotationsParamns) {
-        var annotationsParamns = _annotationsParamns;
+    var objectGeneration = function (_annotationsParamns, _sufix) {
         var object = {};
+        var sufix = _sufix;
+        var annotationsParamns = _annotationsParamns.map(function (item, index) { return (index % 3 === 0) ? item.toLowerCase() : item });
+        var keySufix = annotationsParamns.indexOf('sufix=');
+        if (keySufix !== -1) {
+            sufix = _sufix + annotationsParamns[keySufix + 1].replace(/\"/g, "");
+        }
         for (var key in annotationsParamns) {
             key = parseInt(key);
             var keyObj = null;
             if (annotationsParamns.hasOwnProperty(key) && key % 3 === 0) {
                 keyObj = annotationsParamns[key].replace("=", "").toLowerCase();
-                object[keyObj] = annotationsParamns[key + 1].replace(/\"/g, "");
+                object[keyObj] = annotationsParamns[key + 1].replace(/\"/g, "") + sufix;
                 if (keyObj === "type") {
                     object[keyObj] = object[keyObj].toLowerCase();
                 }
@@ -192,29 +209,11 @@ global.populateToPersistence = function (_variable, _mainAnnotation, _nameVariab
     var nameMainVariable = _nameVariable;
     var nameAnnotation = _annotationParam;
     var stringConcat = _stringConcat;
-    var keyArr = [];
-    var incrementValue = {};
     var string = "bindVars = {";
     for (var key in object) {
         if (object.hasOwnProperty(key) && typeof object[key] !== "function") {
-            if (keyArr.indexOf(object[key][nameAnnotation]) !== 1) {
-                keyArr.push(object[key][nameAnnotation]);
-            }
-            if (typeof incrementValue[object[key][nameAnnotation]] === 'undefined') {
-                incrementValue[object[key][nameAnnotation]] = 1;
-            } else {
-                incrementValue[object[key][nameAnnotation]]++;
-            }
-            var value = null;
-            var keyJoin = object[key][nameAnnotation] + stringConcat + incrementValue[object[key][nameAnnotation]];
-            var increment = incrementValue[object[key][nameAnnotation]] + ".";
-            if (key.indexOf(increment) === -1) {
-                string += "\"" + object[key][nameAnnotation] + "\":{";
-                value = object.getTypeForEval(eval("_variable." + key), object[key].type);
-            } else if (key.indexOf(increment) !== -1) {
-                string += "\"" + keyJoin + "\":{";
-                value = object.getTypeForEval(eval("_variable." + key), object[key].type);
-            }
+            string += "\"" + object[key][nameAnnotation] + "\":{";
+            var value = object.getTypeForEval(eval("_variable." + key), object[key].type);
             string += "\"val\":" + value + ",";
             string += "type:oracledb." + object[key].type.toUpperCase() + ",";
             string += "dir:oracledb." + object[key].dir;
